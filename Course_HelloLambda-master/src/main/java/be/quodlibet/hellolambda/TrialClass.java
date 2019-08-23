a53ccfe3a2412d4277dc6d0a1ee1e6caa2eae716
Gupta <guptamoh@pun-guptamoh-m.ds.ad.adp.com>
2019-08-23 15:17:16 +0530

    first

---
+++ Course_HelloLambda-master/src/main/java/be/quodlibet/hellolambda/TrialClass.java
@@ -0,0 +1,10 @@
+package be.quodlibet.hellolambda;
+
+public class TrialClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Course_HelloLambda-master/src/main/java/be/quodlibet/hellolambda/helloHandler.java
@@ -0,0 +1,24 @@
+package be.quodlibet.hellolambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/**
+ *
+ * @author Dries Horions <dries@quodlibet.be>
+ */
+public class helloHandler implements RequestHandler<helloInput, helloOutput>
+{
+
+    @Override
+    public helloOutput handleRequest(helloInput i, Context cntxt)
+    {
+    	//InsertCustomer in = new InsertCustomer();
+        helloOutput o = new helloOutput();
+        o.setMessage("Hello " + i.getName());
+        o.setFunctionName(cntxt.getFunctionName());
+        o.setMemoryLimit(cntxt.getMemoryLimitInMB());
+        cntxt.getLogger().log(i.getName() + " said hello");
+        return o;
+    }
+}
